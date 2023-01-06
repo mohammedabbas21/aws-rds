0a1a95dc90405ab139505f09484851b7afd58cf8
Mohammed Abbas <abbas.cloud21@gmail.com>
2023-01-05 19:24:06 -0500

    Documented APIs using Swagger Open API

--- src/main/java/com/example/awsrds/AwsRdsApplication.java
+++ src/main/java/com/example/awsrds/AwsRdsApplication.java
@@ -3,7 +3,15 @@ package com.example.awsrds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+
+
+
 @SpringBootApplication
+//@OpenAPIDefinition(info = @Info(title="Address API",version="1.7",description="Address API"))
 public class AwsRdsApplication {
 
 	public static void main(String[] args) {
