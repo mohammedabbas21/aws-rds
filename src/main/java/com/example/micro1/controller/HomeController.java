001372351c7b39f6fab583fa9c9b25201d79ad5a
Mohammed Abbas <abbas.cloud21@gmail.com>
2023-01-09 15:55:12 -0500

    Completed fetching data from aws-rds microservice

--- src/main/java/com/example/micro1/controller/HomeController.java
+++ src/main/java/com/example/micro1/controller/HomeController.java
@@ -1,10 +1,14 @@
 package com.example.micro1.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.micro1.models.Sales;
 
 
 @Controller
@@ -13,6 +17,16 @@ public class HomeController {
 
 	@GetMapping("/getalldata")
 	public String getAllData(Model model) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		ResponseEntity<Sales[]> response =
+				  restTemplate.getForEntity(
+				  "http://localhost:8080/sales/getall",
+				  Sales[].class);
+				Sales[] listSales = response.getBody();
+				
+		model.addAttribute("listSales", listSales);
 		return "index";
 	}
 	
